bd4585ebb08b6439e9470c323a4d10dd51fc11e7
Bhavya Wahie <bhavyawahie@gmail.com>
2021-08-05 18:41:11 +0530

    frontend connected with backend -product screen pending

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const products = require('./data/products.js');
+
+dotenv.config( {path: './config/.env'} );
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if(process.env.NODE_ENV === "development") {
+    app.use(morgan('dev'))
+}
+
+app.get("/", (req, res) => {
+    res.send("API in work!")
+})
+
+app.get("/api/products", (req, res) => {
+    res.status(200).json(products)
+})
+
+app.get("/api/products/:id", (req, res) => {
+    const fetchProduct = products.find(p => p._id === req.params.id)
+    res.status(200).json(fetchProduct)
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server Started running in ${process.env.NODE_ENV} mode at http://localhost:${PORT}`.blue.bold)
+});
