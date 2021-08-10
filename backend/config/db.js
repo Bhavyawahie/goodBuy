380a0bf8a964b7166a7853f944455e0cf7136906
Bhavya Wahie <bhavyawahie@gmail.com>
2021-08-10 22:32:10 +0530

    usecreateindex set to true(depraction warning)

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
         const connection = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useFindAndModify: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true, 
+            useCreateIndex: true
         })
         console.log(`MongoDB server connected : ${connection.connection.host}`.green.inverse);
     }
