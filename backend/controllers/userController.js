45747da7e8305011182b07fd2450bd652c5a4141
Bhavya Wahie <bhavyawahie@gmail.com>
2021-08-18 01:48:47 +0530

    new routes and corresponding controllers added with password hashing capability middleware in schema

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel')
-
+const generateToken = require('../utils/generateToken')
 
 //  @desc    Auth user and get token
 //  @route   POST /api/users/login
@@ -16,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin : user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     }
     else {
@@ -25,6 +25,63 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+
+//  @desc    Register a new user
+//  @route   POST /api/users
+//  @access  Public
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+    const userExists = await User.findOne({email})
+
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin : user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
+
+//  @desc    get user profile
+//  @route   GET /api/users/profile
+//  @access  Protected
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin : user.isAdmin,
+        })
+    }
+    else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 module.exports = {
-    authUser
+    authUser,
+    registerUser,
+    getUserProfile
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { authUser } = require('../controllers/userController')
+const { authUser, getUserProfile, registerUser } = require('../controllers/userController')
+const { protect } = require('../middlewares/authMiddleware')
 
+router.route("/").post(registerUser)
 router.post("/login", authUser)
+router.route("/profile").get(protect, getUserProfile)
 
 
 module.exports = router;
