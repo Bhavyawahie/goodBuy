92abacefae33d2ededa4f8d04b3d44332ce67261
Bhavya Wahie <bhavyawahie@gmail.com>
2021-08-19 17:14:53 +0530

    ability to put request and make change to user profile added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -45,7 +45,7 @@ const registerUser = asyncHandler(async (req, res) => {
     })
 
     if(user) {
-        res.status(200).json({
+        res.status(201).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -80,8 +80,38 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
+//  @desc    UPDATE user profile
+//  @route   PUT /api/users/profile
+//  @access  PRIVATE
+
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if(req.body.password){
+            user.password = req.body.password
+        }
+        const updatedUser = await user.save()
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin : updatedUser.isAdmin,
+            token: generateToken(updatedUser._id)
+        }) 
+    }
+    else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 module.exports = {
     authUser,
     registerUser,
-    getUserProfile
+    getUserProfile,
+    updateUserProfile
 }
