f0c975a093ddbbc2fc0fc74b8bbf7748ed3da10f
Bhavya Wahie <bhavyawahie@gmail.com>
2021-08-23 22:15:27 +0530

    1. orderScreen action,reducer,constants,store and frontend created
    2. orderscreen and order/:id route added to app.jsx
    
    orderDetails action, reducer, constants added to existing placeOrder screen frontend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,7 +54,32 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    update isPaid status of the order by it's Id 
+// @route   PUT /api/orders/:id/pay
+// @access  Protected
+
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+    if(order) {
+        order.isPaid = true,
+        order.paidAt = Date.now(),
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.email_address
+        }
+    const updatedOrder = await order.save()
+    res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order Not Found!')
+    }
+})
+
 module.exports = {
     addOrderItems,
-    getOrderById
+    getOrderById,
+    updateOrderToPaid
 }
