2797386b566c935e39bd2ed5ce153a8259516744
Bhavya Wahie <bhavyawahie@gmail.com>
2021-08-27 02:41:00 +0530

    admin only user route created that fetches all user's info

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -109,9 +109,19 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
+//  @desc    Get all users
+//  @route   GET /api/users
+//  @access  Private (admin-only)
+
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({})
+    res.json(users)
+})
+
 module.exports = {
     authUser,
     registerUser,
     getUserProfile,
-    updateUserProfile
+    updateUserProfile,
+    getUsers
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { authUser, getUserProfile, registerUser, updateUserProfile } = require('../controllers/userController')
-const { protect } = require('../middlewares/authMiddleware')
+const { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } = require('../controllers/userController')
+const { protect, admin } = require('../middlewares/authMiddleware')
 
-router.route("/").post(registerUser)
+router.route("/").post(registerUser).get(protect, admin, getUsers)
 router.post("/login", authUser)
 router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
 
