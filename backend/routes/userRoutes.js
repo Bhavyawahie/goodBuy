417a03f4a432cf85ef620ab8d419c3bb4b030a92
Bhavya Wahie <bhavyawahie@gmail.com>
2021-09-08 21:49:31 +0530

    unused dependencies removed, admin header tweaked, iphone images changed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,19 +1,18 @@
 const express = require('express');
-const { get } = require('mongoose');
 const router = express.Router();
 const { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser } = require('../controllers/userController')
 const { protect, admin } = require('../middlewares/authMiddleware')
 
 router.route("/").post(registerUser).get(protect, admin, getUsers)
 router.post("/login", authUser)
-router
+router                                        // User specific indivisual
     .route("/profile")
     .get(protect, getUserProfile)
-    .put(protect, updateUserProfile);
-router
+    .put(protect, updateUserProfile); 
+router                                      // Admin-Only Private Routes
     .route("/:id")
     .get(protect, admin, getUserById)
     .put(protect, admin, updateUser)
-    .delete(protect, admin, deleteUser)
+    .delete(protect, admin, deleteUser);
 
 module.exports = router;
