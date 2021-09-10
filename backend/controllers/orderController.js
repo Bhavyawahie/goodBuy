53a7a1f848778064c21c31a01d29df65d0af8ac8
Bhavya Wahie <bhavyawahie@gmail.com>
2021-09-11 02:16:16 +0530

    mark as delivered (admin only) added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -62,16 +62,33 @@ const getOrderById = asyncHandler(async (req, res) => {
 const updateOrderToPaid = asyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id)
     if(order) {
-        order.isPaid = true,
-        order.paidAt = Date.now(),
+        order.isPaid = true
+        order.paidAt = Date.now()
         order.paymentResult = {
             id: req.body.id,
             status: req.body.status,
             update_time: req.body.update_time,
             email_address: req.body.email_address
         }
-    const updatedOrder = await order.save()
-    res.json(updatedOrder)
+        const updatedOrder = await order.save()
+        res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order Not Found!')
+    }
+})
+// @desc    update isDelivered status of the order by it's Id 
+// @route   PUT /api/orders/:id/deliver
+// @access  Private (admin-only)
+
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+    if(order) {
+        order.isDelivered = true
+        order.deliveredAt = Date.now()
+        const updatedOrder = await order.save()
+        res.json(updatedOrder)
     } else {
         res.status(404)
         throw new Error('Order Not Found!')
@@ -101,6 +118,7 @@ module.exports = {
     addOrderItems,
     getOrderById,
     updateOrderToPaid,
+    updateOrderToDelivered,
     getMyOrders,
     getOrders
 }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders } = require('../controllers/orderController')
+const { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders, updateOrderToDelivered } = require('../controllers/orderController')
 const { protect, admin } = require('../middlewares/authMiddleware')
 
 router.route("/").get(protect, admin, getOrders).post(protect, addOrderItems)
 router.route("/myorders").get(protect, getMyOrders)
 router.route("/:id").get(protect, getOrderById)
 router.route("/:id/pay").put(protect, updateOrderToPaid)
+router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered)
 
 
 module.exports = router;
