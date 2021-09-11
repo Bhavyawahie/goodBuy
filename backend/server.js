f68ff70ab1a17ef2be742e343bbc04c93f1499a2
Bhavya Wahie <bhavyawahie@gmail.com>
2021-09-12 03:20:18 +0530

    v.1.0.0

--- backend/server.js
+++ backend/server.js
@@ -26,12 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 if(process.env.NODE_ENV === "development") {
     app.use(morgan('dev'))
 }
-
-app.get("/", (req, res) => {
-    res.send("API in work!")
-})
 app.use("/api/upload", uploadRoutes)
-
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
@@ -39,6 +34,15 @@ app.get("/api/config/razorpay", (req, res) => res.send(process.env.RAZORPAY_API_
 app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
 app.use(notFound)
 app.use(errorHandler)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'frontend', 'build', 'index.html')))
+} else {
+    app.get("/", (req, res) => {
+        res.send("API in work!")
+    })
+}
 
 const PORT = process.env.PORT || 4000;
 
