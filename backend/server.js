3fb4d3da600d7ffd5b30111212c9159e203e00b9
Bhavya Wahie <bhavyawahie@gmail.com>
2021-09-12 22:39:03 +0530

    get to root route ommited

--- backend/server.js
+++ backend/server.js
@@ -36,12 +36,8 @@ app.use(errorHandler)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
-    } else {
-    app.get('/', (req, res) => {
-        res.send('API is running....')
-    })
-    }
-
+}
+    
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
