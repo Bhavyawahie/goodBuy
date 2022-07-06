105a93a056fa36ff06ce08a7e97f027edb678283
Bhavya Wahie <bhavyawahie@gmail.com>
2022-07-06 22:45:52 +0530

    size limit of chunk from request object increased to 50mb limit

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 const app = express();
 
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true }));
 
 if(process.env.NODE_ENV === "development") {
