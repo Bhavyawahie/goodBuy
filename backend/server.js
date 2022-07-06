581cafdb4b5de82fbe09ee5ba5412e6055d79b29
Bhavya Wahie <bhavyawahie@gmail.com>
2022-07-07 02:08:26 +0530

    routing issue resolved

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.get("/api/config/razorpay", (req, res) => res.send(process.env.RAZORPAY_API_
 app.use('/uploads', express.static('uploads'))
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join('frontend/build')))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve('frontend', 'build', 'index.html')))
 } else {
     app.get("/", (req, res) => {
         res.send("API in work!")
