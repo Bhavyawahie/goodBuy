df2ee42ce233f4e528afd75bb06c6697f4f04f43
Bhavya Wahie <bhavyawahie@gmail.com>
2022-07-06 19:46:23 +0530

    new route to post product images for updation addded

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview} = require('../controllers/productController')
+const { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview, uploadProductImage} = require('../controllers/productController')
 const { protect, admin } = require('../middlewares/authMiddleware')
 
 router
@@ -17,5 +17,9 @@ router
     .get(getProductById)
     .put(protect, admin, updateProduct)
     .delete(protect, admin, deleteProduct)
+
+router
+    .route("/:id/image/upload")
+    .post(protect, uploadProductImage)    
 
 module.exports = router;
