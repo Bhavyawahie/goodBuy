b9b535c4542b3b7bf9c1bb923aefc6c62c1854e9
Bhavya Wahie <bhavyawahie@gmail.com>
2023-04-11 01:20:43 +0530

    server and app now created as separate entities

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const morgan = require('morgan')
+
+const {notFound, errorHandler} = require('./middlewares/errorMiddleware')
+
+const productRoutes = require('./routes/productRoutes')
+const userRoutes = require('./routes/userRoutes')
+const orderRoutes = require('./routes/orderRoutes')
+
+const connectDB = require('./config/db.js')
+
+dotenv.config( {path: '/.env'} )
+
+connectDB()
+
+app.use(express.json({limit: '50mb'}))
+app.use(express.urlencoded({ extended: true }))
+
+if(process.env.NODE_ENV === "development") {
+    app.use(morgan('dev'))
+}
+
+app.use("/api/products", productRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/orders", orderRoutes)
+app.get("/api/config/razorpay", (req, res) => res.send(process.env.RAZORPAY_API_KEY))
+if(process.env.NODE_ENV === 'production') {
+    app.get("/", (req, res) => {
+        res.send("API in work!")
+    })
+}
+app.use(notFound)
+app.use(errorHandler)
+
+module.exports = app
--- server.js
+++ server.js
@@ -1,51 +1,8 @@
-const path = require('path')
-const express = require('express')
-const dotenv = require('dotenv')
-const colors = require('colors')
-const morgan = require('morgan')
-const {notFound, errorHandler} = require('./middlewares/errorMiddleware')
-const productRoutes = require('./routes/productRoutes')
-const userRoutes = require('./routes/userRoutes')
-const orderRoutes = require('./routes/orderRoutes')
-
-const connectDB = require('./config/db.js')
-
-
-dotenv.config( {path: '/.env'} )
-
-connectDB()
-const app = express()
-
-
-app.use(express.json({limit: '50mb'}))
-app.use(express.urlencoded({ extended: true }))
-
-if(process.env.NODE_ENV === "development") {
-    app.use(morgan('dev'))
-}
-
-
-app.use("/api/products", productRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/orders", orderRoutes)
-app.get("/api/config/razorpay", (req, res) => res.send(process.env.RAZORPAY_API_KEY))
-app.use('/uploads', express.static('uploads'))
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join('frontend/build')))
-    app.get('*', (req, res) => res.sendFile(path.resolve('frontend', 'build', 'index.html')))
-} else {
-    app.get("/", (req, res) => {
-        res.send("API in work!")
-    })
-}
-app.use(notFound)
-app.use(errorHandler)
-
-
-
-
+const app = require('./app')
+const http = require('http')
+const server = http.createServer(app)
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server Started running in ${process.env.NODE_ENV} mode at http://localhost:${PORT}`.yellow.bold.inverse)
 })
